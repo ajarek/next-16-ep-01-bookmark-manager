99f623237c6ce9675c28ac6cb836461a90090ba4
Ajarek <ajarek@poczta.onet.pl>
2025-10-29 00:38:15 +0100

    added data.json

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,5 +1,5 @@
 import {
- Sidebar,
+  Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
@@ -7,33 +7,31 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+  SidebarSeparator,
+  SidebarHeader,
+} from '@/components/ui/sidebar'
+import { Home, Archive } from 'lucide-react'
+import { Input } from './ui/input'
 const items = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    title: 'AI',
+    numberOfTabs: 2,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    title: 'Community',
+    numberOfTabs: 5,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    title: 'CSS',
+    numberOfTabs: 4,
   },
   {
-    title: "Search",
-    url: "#",
-    icon: Search,
+    title: 'Design',
+    numberOfTabs: 1,
   },
   {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: 'HTML',
+    numberOfTabs: 0,
   },
 ]
 export function AppSidebar() {
@@ -44,14 +42,41 @@ export function AppSidebar() {
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href='#'>
+                    <Home />
+                    <span className='text-base'>Home</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href='#'>
+                    <Archive />
+                    <span className='text-base'>Archived</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarSeparator />
+              <SidebarHeader>Tags</SidebarHeader>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
+                <SidebarMenuItem
+                  key={item.title}
+                  className='flex items-center justify-between mb-2'
+                >
+                  <div className='flex items-center gap-2'>
+                    <Input
+                      type='checkbox'
+                      id={item.title}
+                      className=' w-4 h-4 '
+                    />
+
+                    <span className='text-base'>{item.title}</span>
+                  </div>
+                  <div className='w-6 h-6 flex items-center justify-center rounded-full text-base bg-gray-300 text-black'>
+                    {item.numberOfTabs}
+                  </div>
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
